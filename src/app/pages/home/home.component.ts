4e49a58e2daeade2c9da0512cf0a58e78364cef5
JaydGuedes <jayd.paiva@gmail.com>
2023-06-29 21:40:56 -0300

    cont

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  articles: any;
+  mostViewed: any;
+  env = environment;
+
+  constructor(private http: HttpClient) {
+    this.getAllArticles();
+    this.getMostViewed(5);
+  }
+
+  ngOnInit() { }
+
+  getAllArticles() {
+    this.http.get(`${this.env.apiBaseURL}/articles`)
+      .subscribe((response) => {
+        this.articles = response;
+      },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+
+  getMostViewed(limit: number) {
+    this.http.get(`${this.env.apiBaseURL}/articles/views/${limit}`)
+      .subscribe((response) => {
+        this.mostViewed = response;
+      },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
 
 }
